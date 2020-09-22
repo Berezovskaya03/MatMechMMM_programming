36719f5d666d450bfdd1d3fd6d2e600c28e5c869
Berezovskaya03 <Berezovskaya03@users.noreply.github.com>
2020-09-22 21:35:35 +0300

    fjsejjsfsjkfd

---
+++ 2020.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+    int main() {
+        int a; //количество чисел
+    cin >> a;
+    int b=0;
+    while ( a>0) {
+          cin >> int c;// число из последовательности
+          a=a-1;
+          b=b+с*с*с;} 
+    cout << b;
+    }
