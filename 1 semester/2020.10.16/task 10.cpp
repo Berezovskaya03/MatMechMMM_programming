97ed17c194a1a7138cb70b67e913feeb6a012c0e
Berezovskaya03 <71541304+Berezovskaya03@users.noreply.github.com>
2020-10-28 19:35:25 +0300

    Update task 10.cpp

--- 1 semester/2020.10.16/task 10.cpp
+++ 1 semester/2020.10.16/task 10.cpp
@@ -1,13 +1,13 @@
-//возведение числа в степень по модулю
+//возведение числа в степень
 # include <iostream>
 using namespace std;
-int binpow (int x, int n, int mod) {
-if (n==0) return 0;
-if (n%2) return (x%mod)*binpow(x, n-1, mod);
-if (!n%2) return (x%mod)*binpow (x*x, n-1, mod);
+int binpow (int x, int n) {
+if (n==0) return 1;
+if (n%2==1) return x*binpow(x, n-1);
+if (n%2==0) {int b=binpow (x, n/2);
+return b*b;}
 }
 int main(){
-int x1, n1, mod1;
-cin >> x1 >> n1 >> mod1;
-binpow (x1, n1, mod1);
-cout<<(x1%mod1);}
+int x1, n1;
+cin >> x1 >> n1;
+cout<<binpow (x1, n1);}
