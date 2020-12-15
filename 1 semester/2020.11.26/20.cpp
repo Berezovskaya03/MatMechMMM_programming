e101011e1cacd760cb6dc619658b726fd2fe18d6
Berezovskaya03 <71541304+Berezovskaya03@users.noreply.github.com>
2020-12-15 16:29:50 +0300

    Update 20.cpp

--- 1 semester/2020.11.26/20.cpp
+++ 1 semester/2020.11.26/20.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int c[n-1][m-1];
-    int dp[n-1][m-1];
+    int c[n-1][m-1];//цена клетки
+    int dp[n-1][m-1];//цена пути до клетки
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> c[i][j];
@@ -15,17 +15,14 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (i || j) {
-                if (i - 1 >= 0) {
-                    dp[i][j] = max(dp[i][j], dp[i - 1][j] + c[i][j]);
+                if ((i-1>=0)&&(j-1>=0)) {
+                dp[i][j]=min(dp[i-1][j], dp[i][j - 1]);
+                dp[i][j]=min(dp[i][j], dp[i-1][j - 1])+c[i][j];
                 }
 
-                if (j - 1 >= 0) {
-                    dp[i][j] = max(dp[i][j], dp[i][j - 1] + c[i][j]);
-                }
-            }
+        if ((i-1>=0)&&(j==0)) dp[i][j] = dp[i-1][j] + c[i][j];
+        if ((j-1>=0)&&(i==0)) dp[i][j] = dp[i][j - 1] + c[i][j];
+             }
         }
-    }
-
     cout << dp[n - 1][m - 1];
 }
